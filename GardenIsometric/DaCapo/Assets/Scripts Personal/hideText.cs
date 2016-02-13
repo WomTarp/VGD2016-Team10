3e44795a39d992dc2b6326297169a755849cfe31
ilimjo1 <ilimjoco21@gmail.com>
2016-02-13 17:49:48 -0600

    GardenIsometric_scene
    gets closer to certain objects text appears

---
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/hideText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hideText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        gameObject.SetActive(false);
+	}
+	
+	
+}
---
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/move.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+          if (Input.GetKey(moveUp))
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 3, 0);
+          else if (Input.GetKey(moveDown))
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3, 0);
+          else if (Input.GetKey(moveLeft))
+                GetComponent<Rigidbody2D>().velocity = new Vector3(-3, 0, 0);
+          else if (Input.GetKey(moveRight))
+                GetComponent<Rigidbody2D>().velocity = new Vector3(3, 0, 0);
+          else 
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+    }
+}
+
+
