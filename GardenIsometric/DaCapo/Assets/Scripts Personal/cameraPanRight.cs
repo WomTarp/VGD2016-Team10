fa3a7ee086fd4fab2acbdb8b7eef20725b557cb2
ilimjo1 <ilimjoco21@gmail.com>
2016-02-19 16:05:45 -0600

    simple platform level w/ pan right cam

---
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/cameraPanRight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraPanRight : MonoBehaviour
+{
+
+private float speed = 2.0f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))        
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(3, 0, 0);
+            //if (bool player = renderer.isVisible)
+        }           
+    }
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "MainCharacter")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GardenPlatformLevel");
+        }
+    }
+}
---
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/notes_pickup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class notes_pickup : MonoBehaviour {
+
+    public AudioSource sound1;
+  
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "MainCharacter")
+        {
+            sound1.Play();
+            gameObject.SetActive(false);
+        }
+    }
+}
