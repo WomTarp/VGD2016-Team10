72155cb42fd1cc130b99c5b3a1b7cc51b723cfb3
Kevin Hartline <khartntw@bellsouth.net>
2016-02-19 13:58:38 -0600

    Added Doris' sprite animation for the main character's downward walk cycle. Also, adjusted main character's walking speed

--- GardenIsometric/DaCapo/Assets/Scripts Personal/move.cs
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/move.cs
@@ -7,25 +7,25 @@ public class move : MonoBehaviour {
     public KeyCode moveDown;
     public KeyCode moveLeft;
     public KeyCode moveRight;
+    public int speed;
 
     void Start()
     {
-
     }
 
 
     void FixedUpdate()
     {
           if (Input.GetKey(moveUp))
-                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 3, 0);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
           else if (Input.GetKey(moveDown))
-                GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3, 0);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
           else if (Input.GetKey(moveLeft))
-                GetComponent<Rigidbody2D>().velocity = new Vector3(-3, 0, 0);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
           else if (Input.GetKey(moveRight))
-                GetComponent<Rigidbody2D>().velocity = new Vector3(3, 0, 0);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
           else 
-                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
     }
 }
 
