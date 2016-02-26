dc360659275678c9e86477e4be69e223aa8524b5
Kevin Hartline <khartntw@bellsouth.net>
2016-02-26 15:18:35 -0600

    Added a Scene Transition

---
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/GoInside.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GoInside : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Girl")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
