c16d15db11459f70e350e0b8ba6986485652edc3
WomTarp <womtarp1@gmail.com>
2016-03-17 15:47:42 -0500

    Made Dialogue Adjustments
    Tuned up the dialogue scene so the boxes would match the ones in the
    isometric world. Also optimized code a bit.

--- GardenIsometric/DaCapo/Assets/DialoguePackage/FakeDialogueScript.cs
+++ GardenIsometric/DaCapo/Assets/DialoguePackage/FakeDialogueScript.cs
@@ -5,15 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class FakeDialogueScript : MonoBehaviour
 {
-    public Sprite line1;
-    public Sprite line2;
-    public Sprite line3;
-    public Sprite line4;
-    public Sprite line5;
-    public Sprite line6;
-    public Sprite line7;
-    public Sprite line8;
-    public Sprite line9;
+    public Text instruction;
+    public Image background;
+    public Text lines;
+    public TextAsset line1;
+    public TextAsset line2;
+    public TextAsset line3;
+    public TextAsset line4;
+    public TextAsset line5;
+    public TextAsset line6;
+    public TextAsset line7;
+    public TextAsset line8;
+    public TextAsset line9;
     public Sprite doorOpen;
     public Sprite doorClosed;
     public int lineCount;
@@ -22,8 +25,11 @@ public class FakeDialogueScript : MonoBehaviour
 	void Start ()
     {
         lineCount = 1;
-        GameObject.Find("background").GetComponent<Image>().sprite = doorOpen;
-        GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line1;
+        instruction = instruction.GetComponent<Text>();
+        background = background.GetComponent<Image>();
+        lines = lines.GetComponent<Text>();
+        background.sprite = doorOpen;
+        lines.text = line1.ToString();
     }
 	
 	// Update is called once per frame
@@ -40,43 +46,45 @@ public class FakeDialogueScript : MonoBehaviour
     {
         if (lineCount == 2)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line2;
+            instruction.enabled = false;
+            lines.text = line2.ToString(); ;
+            
         }
 
         else if (lineCount == 3)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line3;
+            lines.text = line3.ToString(); ;
         }
 
         else if (lineCount == 4)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line4;
+            lines.text = line4.ToString(); ;
         }
 
         else if (lineCount == 5)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line5;
+            lines.text = line5.ToString(); ;
         }
         
         else if (lineCount == 6)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line6;
+            lines.text = line6.ToString(); ;
         }
 
         else if (lineCount == 7)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line7;
+            lines.text = line7.ToString(); ;
         }
 
         else if (lineCount == 8)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line8;
-            GameObject.Find("background").GetComponent<Image>().sprite = doorClosed;
+            lines.text = line8.ToString(); ;
+            background.sprite = doorClosed;
         }
 
         else if (lineCount == 9)
         {
-            GameObject.Find("dialogueBox").GetComponent<Image>().sprite = line9;
+            lines.text = line9.ToString(); ;
         }
 
         else if (lineCount == 10)
