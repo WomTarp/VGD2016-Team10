1893faed90d5db8df2e47552a88eb5e3f3cefc44
WomTarp <womtarp1@gmail.com>
2016-04-20 23:53:44 -0500

    Added Dog Animation
    The dog in the exterior scene walks now instead of floating.

--- GardenIsometric/DaCapo/Assets/Scripts Personal/DogMove.cs
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/DogMove.cs
@@ -8,10 +8,13 @@ public class DogMove : MonoBehaviour {
     public float speed;
     public TextBoxManager tBox;
     public bool addDescription;
+    public Animator anim;
 
-	void Start () {
+    void Start () {
         movePoint = GameObject.Find("DogMovePoint").transform;
         tBox = GameObject.Find("TextBoxManager").GetComponent<TextBoxManager>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("right", true);
         canMove = false;
         addDescription = false;
 	}
@@ -21,12 +24,14 @@ public class DogMove : MonoBehaviour {
         if (transform.position != movePoint.position && canMove && !tBox.isActive)
         {
             Vector2 move = Vector2.MoveTowards(transform.position, movePoint.position, speed);
+            anim.SetBool("walkRight", true);
             GetComponent<Rigidbody2D>().MovePosition(move);
         }
 
         if (transform.position == movePoint.position)
         {
             canMove = false;
+            anim.SetBool("walkRight", false);
             GetComponent<Collider2D>().isTrigger = false;
             addDescription = true;
         }
