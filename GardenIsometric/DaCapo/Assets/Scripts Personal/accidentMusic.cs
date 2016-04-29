c3a12913fec9e034d4ce5ae3ae1090407307e512
WomTarp <womtarp1@gmail.com>
2016-04-29 12:48:44 -0500

    Final Push
    Added last music cues

--- GardenIsometric/DaCapo/Assets/Scripts Personal/accidentMusic.cs
+++ GardenIsometric/DaCapo/Assets/Scripts Personal/accidentMusic.cs
@@ -27,8 +27,7 @@ public class accidentMusic : MonoBehaviour {
     {
         if (level == 15 || level == 0 || level == 1 || level == 2 || level == 3 || level == 4
             || level == 5 || level == 6 || level == 7 || level == 9 || level == 10 || level == 11
-            || level == 13 || level == 16 
-            || level == 8 || level == 19)
+            || level == 13 || level == 16 || level == 8 || level == 18 || level == 19)
             Destroy(musicPlayer);
     }
 }
